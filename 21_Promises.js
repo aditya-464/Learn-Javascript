6b49a0b77d79a6d737c2a06c8a8fd095360fb00d
Aditya <adityagiri1911@gmail.com>
2022-09-25 08:50:51 +0530

    Removed flag from for loop and edited comments in Promises.js

--- 21_Promises.js
+++ 21_Promises.js
@@ -1,20 +1,16 @@
 // Promises Using JS
+// Promises are asynchronous in nature, i.e they take time to resolve or reject. Meanwhile, the other lines present in the code continue their execution
 const numList = [2, 3, 5, 7, 9, 11, 13, 17, 19, 23];
 
 // Creating a promise to check the availability of 9 in numList
 const myPromise = new Promise((resolve, reject) => {
-    let flag = false;
     for (let i = 0; i < numList.length; i++) {
         if (i == 9) {
-            flag = true;
+            resolve("Number 9 is Present in the numList!");
         }
     }
-    if (flag) {
-        resolve("Number 9 is Present in the numList!");
-    }
-    else {
-        reject("Number 9 is Absent in the numList!");
-    }
+    reject("Number 9 is Absent in the numList!");
+
 });
 
 // If 9 is present in numList, resolve() is called which then triggers the "then" part
